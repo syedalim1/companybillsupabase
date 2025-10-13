7ff7814c5816508396d5d5b95b14bd8a3fb8182a
SYED ALI M <143418859+syedalim1@users.noreply.github.com>
2025-10-13 11:26:36 +0530

    Update InvoiceContent.jsx

--- src/components/invoice/InvoiceContent.jsx
+++ src/components/invoice/InvoiceContent.jsx
@@ -5,8 +5,8 @@ import InvoiceFooter from './InvoiceFooter';
 
 const InvoiceContent = ({ copyType, invoiceData, subtotal, cgstAmount, sgstAmount, igstAmount, grandTotal, mode, gstOption }) => {
     return (
-        <div className="p-2 bg-white shadow-2xl print:shadow-none print:p-4 font-sans">
-            <div className=" p-2 flex flex-col "> {/* A4-like container with fixed border */}
+        <div className=" bg-white shadow-2xl print:shadow-none print:p-4 font-sans">
+            <div className=" flex flex-col "> {/* A4-like container with fixed border */}
                 <InvoiceHeader copyType={copyType} invoiceData={invoiceData} mode={mode} />
                 <InvoiceMain invoiceData={invoiceData} subtotal={subtotal} cgstAmount={cgstAmount} sgstAmount={sgstAmount} igstAmount={igstAmount} grandTotal={grandTotal} mode={mode} gstOption={gstOption} />
                 <InvoiceFooter invoiceData={invoiceData} subtotal={subtotal} cgstAmount={cgstAmount} sgstAmount={sgstAmount} igstAmount={igstAmount} grandTotal={grandTotal} mode={mode} gstOption={gstOption} />
